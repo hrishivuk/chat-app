ba945d3088a78b1b8697df3ec1be723b26a3bc5d
Hrishikesh Varma <hrizhi@Hrishikeshs-Laptop.local>
2024-08-20 16:43:27 +0530

    updated color fix

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -5,7 +5,11 @@ const MyMessage = ({ message }) => {
         src={message.attachments[0].file}
         alt="message-attachment"
         className="message-image"
-        style={{ float: "right" }}
+        style={{
+          float: "right",
+          color: "#e7ecef",
+          backgroundColor: "#6096ba",
+        }}
       />
     );
   }
@@ -16,8 +20,8 @@ const MyMessage = ({ message }) => {
       style={{
         float: "right",
         marginRight: "18px",
-        color: "##e7ecef",
-        backgroundColor: "##6096ba",
+        color: "#e7ecef",
+        backgroundColor: "#6096ba",
       }}
     >
       {message.text}
